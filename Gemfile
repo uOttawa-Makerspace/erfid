4a52d18dcd5fbcf96910211ab5cdec48c3351e5f
Wesley Ellis <tahnok@gmail.com>
2015-08-15 16:13:31 -0400

    clean up code some

--- Gemfile
+++ Gemfile
@@ -4,4 +4,3 @@ source "https://rubygems.org"
 gem "faraday"
 gem "nfc"
 gem "daemons"
-
--- erfid.rb
+++ erfid.rb
@@ -2,46 +2,88 @@ require 'json'
 require 'faraday'
 require 'nfc'
 
-def config
-  @config ||= JSON.parse(File.read("config.json"))
+## ERROR CODES
+NO_TAG = -90
+DISCONNECTED = -1
+
+def read_config
+  JSON.parse(File.read("config.json"))
 end
 
-def read_card
-  return "deadbeef"
-  tag = @device.select #will block
-  return tag.to_s
+def get_reader
+  reader = NFC::Context.new.open(nil)
+  log("Connected to #{reader.name}")
+  return reader
+rescue RuntimeError => e
+  error(e)
 end
 
-def send_card(card_number)
-  response = Faraday.post(
+def read_card(reader)
+  tag = reader.poll(10,1)
+  if tag == NO_TAG
+    return nil
+  elsif tag == DISCONNECTED
+    error("Card reader error, shutting down")
+  else
+    return tag.to_s
+  end
+end
+
+def send_card(card_number, config)
+  Faraday.post(
     config["url"],
     { rifd: card_number }
   )
-  response.success?
 end
 
 def log(message)
   puts "#{Time.now} | #{message}"
 end
 
-def setup
-  @context = NFC::Context.new
-  @reader = @context.open(nil)
+def error(message)
+  log("ERROR: " + message)
+  exit(-1)
 end
 
+#catch interupt and log shutdown
 trap('INT') {
   log("Shutting down")
   exit
 }
 
-#setup
+#######################################
+#######################################
+#                                     #
+#             MAIN STUFF HERE         #
+#                   |                 #
+#                   |                 #
+#                   V                 #
+#                                     #
+#######################################
+#######################################
 
-loop do
-  card = read_card
-  log("Read card: #{card}")
-  if send_card(card)
-    log("Successfully sent #{card}")
-  else
-    log("ERROR: invalid response code for #{card}")
+def main
+  log("Staring up!")
+  config = read_config()
+  reader = get_reader()
+
+  loop do
+    card = read_card(reader)
+
+    next unless card #failed to read card
+
+    log("Read card: #{card}")
+
+    response = send_card(card, config)
+
+    if response.success?
+      log("Successfully sent #{card}")
+    else
+      log("ERROR: got #{response.status} sending #{card}: #{response.body}")
+    end
+    sleep(1) #delay a bit to avoid double send
   end
 end
+
+# IT'S GO TIME
+main()
